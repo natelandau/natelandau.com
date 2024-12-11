f345246d7c2f3f8a9136475c121e1fb3cfe34907
Nathaniel Landau <nate@natelandau.com>
2024-12-11 14:35:05 -0500

    fix: use custom domain for site url

--- publishconf.py
+++ publishconf.py
@@ -6,8 +6,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *  # noqa: F403
 
-# SITEURL = "https://natelandau.com"
-SITEURL = "https://natelandau-com.pages.dev"
+SITEURL = "https://natelandau.com"
 RELATIVE_URLS = False
 GISCUS_DATA_CATEGORY_ID = "DIC_kwDOIM6z9s4CSACn"
 GISCUS_DATA_CATEGORY = "Comments"
